c9193a8a0367638ad8f59f427266826ede967be8
MTrofimov95 <yamakasiad1996@gmail.com>
2025-01-25 03:41:34 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "имя пользователя"
+title = "заголовок заметки"
+content = "описание заметки"
+status = "статус заметки"
+created_date = "25.01.2025"
+issue_date = "31.05.2025"
+print ('имя пользователя:', "username")
+print ("заголовок заметки:", 'title')
+print ("описание заметки:", 'content')
+print ("статус заметки:", 'status')
+print ("25.01.2025:", 'created_date')
+print ("31.05.2025:", 'issue_date')
