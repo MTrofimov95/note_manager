3fa81346a9f1f11f147e4cd6e0256efb555dcde4
MTrofimov95 <yamakasiad1996@gmail.com>
2025-01-26 23:38:05 +0300

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -4,9 +4,9 @@ content = "описание заметки"
 status = "статус заметки"
 created_date = "25.01.2025"
 issue_date = "31.05.2025"
-print ('имя пользователя:', "username")
-print ("заголовок заметки:", 'title')
-print ("описание заметки:", 'content')
-print ("статус заметки:", 'status')
-print ("25.01.2025:", 'created_date')
-print ("31.05.2025:", 'issue_date')
+print ('имя пользователя:', username)
+print ("заголовок заметки:", title)
+print ("описание заметки:", content)
+print ("статус заметки:", status)
+print ("Дата создания заметки: ", created_date)
+print ("Дата истечения заметки:", issue_date)
